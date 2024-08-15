4ca9ba711844801f170d1279ba0caf100c6e7817
NillsLopez <yakusaz17@gmail.com>
2024-08-15 17:17:35 -0400

    completed

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-/* Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
-
-function destructivelyAppendCat (catname) {
-    cats.push(catname);
-}
-
-function destructivelyPrependCat (catname) {
-    cats.unshift(catname);
-}
-
-function destructivelyRemoveLastCat() {
-    cats.pop();
-}
-
-function destructivelyRemoveFirstCat() {
-    cats.shift();
-}
-
-function appendCat (catname) {
-    return [...cats, catname];
-}
-
-function prependCat (catname) {
-    return [catname, ...cats]; 
-}
-
-function removeFirstCat () {
-    return cats.slice(1);
-}
-
-function removeLastCat () {
-    return cats.slice(0,cats.length-1);
-}   
-*/
+// Write your solution here!
 
 const cats = ["Milo", "Otis", "Garfield"];
-const newCatName = "pipo"
 
 function destructivelyAppendCat (newCatName){
     cats.push(newCatName);
